cd9078ee384dce9099bb94559830be38340a602c
portalromOS <portalromOS@gmail.com>
2023-02-17 17:41:04 +0000

    Scroll view with an list view for all locales Highligh of locales Save variable of locale

--- app/src/main/java/com/portalrom/setupwizard/LocaleActivity.java
+++ app/src/main/java/com/portalrom/setupwizard/LocaleActivity.java
@@ -1,16 +1,29 @@
 package com.portalrom.setupwizard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowInsetsController;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.portalrom.setupwizard.Utils.SetupWizardUtils;
 
 public class LocaleActivity extends AppCompatActivity {
 
-    Spinner localeSpinner;
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
+
+    String selectedLocale = "";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,27 @@ public class LocaleActivity extends AppCompatActivity {
 
         final WindowInsetsController insetsController = getWindow().getInsetsController();
         SetupWizardUtils.hideTaskBar(insetsController);
+
+        setLocales();
+
+    }
+
+
+    protected void setLocales(){
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.listLocales);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                selectedLocale = listView.getItemAtPosition(i).toString();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/portalrom/setupwizard/Utils/SetupWizardUtils.java
+++ app/src/main/java/com/portalrom/setupwizard/Utils/SetupWizardUtils.java
@@ -1,7 +1,15 @@
 package com.portalrom.setupwizard.Utils;
 
+import android.graphics.Color;
+import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import com.portalrom.setupwizard.R;
 
 public  class SetupWizardUtils {
 
@@ -9,6 +17,42 @@ public  class SetupWizardUtils {
 
         if (insetsController != null) {
             insetsController.hide(WindowInsets.Type.statusBars());
+        }
+    }
+
+    public  static  void dinamicTextViews(View view){
+        ConstraintLayout localeLayout;
+        localeLayout = view.findViewById(R.id.localeScrollTextParent);
+        ConstraintSet set = new ConstraintSet();
+        set.clone(localeLayout);
+
+        int nButtons = 10;
+        TextView parent = new TextView(view.getContext());
+
+        for(int i = 0;i<nButtons;i++){
+
+            TextView tView = new TextView(view.getContext());
+            tView.setText("Line " + i);
+            tView.setId(View.generateViewId());           // <-- Important
+            tView.setTextColor(Color.WHITE);
+            tView.setTextSize(20);
+            tView.setClickable(true);
+            localeLayout.addView(tView);
+
+            set.constrainWidth(tView.getId(),ConstraintSet.WRAP_CONTENT);
+            set.constrainHeight(tView.getId(),ConstraintSet.WRAP_CONTENT);
+
+            if(i==0){
+                set.connect(tView.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
+                set.connect(tView.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,0);
+            }
+            else {
+                set.connect(tView.getId(), ConstraintSet.TOP, parent.getId(), ConstraintSet.BOTTOM, 0);
+                set.connect(tView.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,0);
+            }
+
+            set.applyTo(localeLayout);
+            parent = tView;
         }
     }
 
